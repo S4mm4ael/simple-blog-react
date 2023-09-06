9cc4d26dff36f88178ca15c664fcebd0c54ab3b6
SEMION KRAPIVIN <homer1996@gmail.com>
2023-09-06 15:56:23 +0200

    feat: basic useState functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 function App() {
+  const [count, setCount] = useState(0)
+  const [value, setValue] = useState("TEXT")
+
+  function add() {
+    setCount(count + 1)
+    console.log(count)
+  }
+
   return (
     <div className="App">
-
-    </div>
+      <h1>App {value}</h1>
+      <div>{count}</div>
+      <input type="text"
+        onChange={event => setValue(event.target.value)}
+        value={value}></input>
+      <button onClick={() => add()}>Add</button>
+    </div >
   );
 }
 
