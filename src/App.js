3b9794d576014fb5dc1bf0f53bfb8ecc60d3181e
Semion Krapivin <homer1996@gmail.com>
2023-09-08 19:21:24 +0200

    feat: add function to sort posts

--- src/App.js
+++ src/App.js
@@ -1,36 +1,48 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './styles/App.css'
 import { useState } from "react";
 
 import PostList from './components/PostList/PostList';
 import PostForm from './components/PostForm/PostForm';
+import SelectRegular from './components/UI/SelectRegular/SelectRegular'
 
 function App() {
   const [posts, setPosts] = useState([
     {
       id: Date.now() + Math.random(),
-      title: "Title 1",
+      title: "Title 4",
       body: "aaaaaaaaaaaaaaaa ffffffffffffffff",
     },
     {
       id: Date.now() + Math.random(),
       title: "Title 2",
-      body: "aaaaaaaaaaaaaaaa ffffffffffffffff",
+      body: "ffffffffffffffff sssssssss",
     },
     {
       id: Date.now() + Math.random(),
       title: "Title 3",
-      body: "aaaaaaaaaaaaaaaa ffffffffffffffff",
+      body: "ffffffffffffffff wwwwwwwwwwwwS",
     },
   ]);
+  const [sortOptions, setSortOptions] = useState([
+    { value: 'title', name: 'By title' }, { value: 'body', name: 'By description' }]);
+
+  const [selectedSort, setSelectedSort] = useState('');
 
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id))
   }
-
+  const handleSortChange = (sort) => {
+    setSelectedSort(sort)
+    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
+  }
   return (
     <div className="App">
-      <PostForm setNewPost={setPosts} posts={posts}></PostForm>
+      <div className='App__wrapper_top'>
+        <PostForm setNewPost={setPosts} posts={posts} />
+        <SelectRegular sortOptions={sortOptions} value={selectedSort} onChange={handleSortChange} />
+      </div>
+
       <PostList posts={posts} removePost={removePost} />
     </div >
   );
