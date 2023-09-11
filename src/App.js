4a7683ddb120a9f4406f3ccce52361a3ea2cd81a
Semion Krapivin <homer1996@gmail.com>
2023-09-12 00:26:25 +0200

    feat: add Header

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import './styles/App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import About from './Pages/About/About';
-import Posts from './Pages/Posts/Posts'
+import Posts from './Pages/Posts/Posts';
+import Header from './components/UI/Header/Header';
 
 function App() {
   return (
     <div className='App'>
+      <Header />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Posts />} />
           <Route path="/about" element={<About />} />
         </Routes>
       </BrowserRouter>
+
     </div>
 
 
---
+++ src/components/UI/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="App__header">
+      <a href="/">main</a>
+      <a href="/about">about</a>
+    </div>
+  );
+}
+
+export default Header;
